b1743d62bfb8cb14f6defae7a49296eea013a66d
msabdeljalil <msabdeljalil@gmail.com>
2014-07-22 14:15:47 -0500

    Altered the databases line in the base.py

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -103,9 +103,9 @@ WSGI_APPLICATION = 'microblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-
 import dj_database_url
-DATABASES = {'default': dj_database_url.config()}
+# DATABASES = {'default': dj_database_url.config()}
+DATABASES['default'] = dj_database_url.config()  # From StackOverflow
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
