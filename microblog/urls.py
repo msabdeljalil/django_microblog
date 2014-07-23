960378aac4929670f748f5248796a00a8cd3a019
msabdeljalil <msabdeljalil@gmail.com>
2014-07-23 02:26:49 -0500

    Added a template view route, template dir, and index.html

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 # Uncomment the next two lines to enable the admin:
 # This allows D to introspect your apps,
 # find the admin.py file in them,
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
 )
